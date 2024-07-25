5f2f0f22491668cc3e24b646390354ce569cdcd8
Saawn <itissaavn@gmail.com>
2024-07-26 00:58:27 +0530

    refactor: Update HTML title and package name, fix font paths in CSS, and optimize build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Adjust based on your needs
+  },
+  // Example of handling static assets more effectively
+  publicDir: "public",
+});
